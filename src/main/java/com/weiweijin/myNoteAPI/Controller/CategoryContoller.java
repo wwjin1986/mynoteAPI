71f989b7a3b387221f7ac340bc94042110851521
Weiwei Jin <weiweijin@proton.attlocal.net>
2019-08-13 14:29:12 -0400

    first deploy

--- src/main/java/com/weiweijin/myNoteAPI/Controller/CategoryContoller.java
+++ src/main/java/com/weiweijin/myNoteAPI/Controller/CategoryContoller.java
@@ -5,26 +5,34 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.weiweijin.myNoteAPI.Model.Category;
+import com.weiweijin.myNoteAPI.Service.CategoryService;
 import com.weiweijin.myNoteAPI.repository.CategoryRepository;
 
 @RestController
 public class CategoryContoller {
+	
 	@Autowired
-	private CategoryRepository categoryRepository;
+	private CategoryService categoryService;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/categories")
 	public List getAllCategories() {
-		return categoryRepository.findAll();
+		return categoryService.getAllCategories();
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value ="/categories")
 	public Category addCategory(@Valid @RequestBody Category category) {
-		return categoryRepository.save(category);
+		return categoryService.addCategory(category);
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/categories/{category}")
+	public List getCategorybyCategory(@PathVariable String category) {
+		return categoryService.getCategoryByCategory(category);
 	}
 }
---
+++ src/main/java/com/weiweijin/myNoteAPI/Service/CategoryService.java
@@ -0,0 +1,37 @@
+package com.weiweijin.myNoteAPI.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.weiweijin.myNoteAPI.Model.Category;
+import com.weiweijin.myNoteAPI.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public List getAllCategories() {
+		return categoryRepository.findAll();
+	}
+	
+	public Category addCategory(Category category) {
+		return categoryRepository.save(category);
+	}
+	
+	public List getCategoryByCategory(String category) {
+		List<Category> categories = new ArrayList<>();
+		Iterator<Category> iterator = categoryRepository.findAll().iterator();
+		while (iterator.hasNext()) {
+			Category c = iterator.next();
+			if (c.getCategory().equals(category)) {
+				categories.add(c);
+			}
+		}
+		return categories;
+	}
+}
