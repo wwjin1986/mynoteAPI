2317490cd646d5e174d5860806b59bca219aa395
Weiwei Jin <weiweijin@proton.attlocal.net>
2019-08-14 15:50:16 -0400

    added junit testing

--- src/test/java/com/weiweijin/myNoteAPI/MyNoteApiApplicationTests.java
+++ src/test/java/com/weiweijin/myNoteAPI/MyNoteApiApplicationTests.java
@@ -1,16 +1,34 @@
 package com.weiweijin.myNoteAPI;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.BeforeClass;
+import org.junit.Test;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.get;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 public class MyNoteApiApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	
+	@BeforeClass
+	public static void init() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 8080;
 	}
+	
+	@Test
+	public void testSetup() {
+		String str = "I am done with Junit setup";
+		assertEquals("I am done with Junit setup", str);
+	}
+	
+	@Test
+	public void testCategoriesFetchesSuccess() {
+		get("/categories")
+		.then()
+		.statusCode(200);
+	}
+	
 
 }
