d53678b1d1d32587dd31cb70d6deb12351eb4861
Weiwei Jin <weiweijin@proton.attlocal.net>
2019-08-14 16:33:21 -0400

    added junit testing

--- src/test/java/com/weiweijin/myNoteAPI/MyNoteApiApplicationTests.java
+++ src/test/java/com/weiweijin/myNoteAPI/MyNoteApiApplicationTests.java
@@ -2,10 +2,15 @@ package com.weiweijin.myNoteAPI;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
-import io.restassured.RestAssured;
-import static io.restassured.RestAssured.get;
+
+import com.jayway.restassured.RestAssured;
+
+import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class MyNoteApiApplicationTests {
@@ -21,14 +26,63 @@ public class MyNoteApiApplicationTests {
 	public void testSetup() {
 		String str = "I am done with Junit setup";
 		assertEquals("I am done with Junit setup", str);
+		}
+	
+	@Test
+	public void testCategoryController() {
+		//Test GET method
+		given()
+		.when()
+		.get("/categories")
+		.then()
+		.statusCode(200);	
+		
+		//Test Post method
+		Map<String, String> category = new HashMap<>();	
+		category.put("topic","junit");
+		category.put("category","testing");
+		int id = given()
+		.contentType("application/json")
+		.body(category)
+		.when().post("/categories").then()
+		.body("topic",equalTo("junit"))
+		.body("category", equalTo("testing"))
+		.extract().path("id");
+		
+		//Test Delete method
+		given().pathParam("id",id)
+		.when()
+		.delete("/categories/{id}")
+		.then()
+		.statusCode(200);				
 	}
 	
 	@Test
-	public void testCategoriesFetchesSuccess() {
-		get("/categories")
-		.then()
-		.statusCode(200);
+	public void testNoteController() {
+		//Test GET method
+				given()
+				.when()
+				.get("/notes")
+				.then()
+				.statusCode(200);	
+				
+//				//Test Post method
+//				Map<String, String> n = new HashMap<>();	
+//				category.put("topic","junit");
+//				category.put("category","testing");
+//				int id = given()
+//				.contentType("application/json")
+//				.body(category)
+//				.when().post("/notes").then()
+//				.body("topic",equalTo("junit"))
+//				.body("category", equalTo("testing"))
+//				.extract().path("id");
+//				
+//				//Test Delete method
+//				given().pathParam("id",id)
+//				.when()
+//				.delete("/notes/{id}")
+//				.then()
+//				.statusCode(200);	
 	}
-	
-
 }
